414210e4478c38e4fc0c4f1aabd2187145881d18
Martin Carrera <mcarrera.92@gmail.com>
2016-05-08 14:20:22 -0300

    Added pre-save in chest-schema to fill arrays with 0s.

--- src/schemas/chest-schema.js
+++ src/schemas/chest-schema.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
+const numberOfArenas = 7;
+
 const ChestSchema = new Schema({
 
   name: {
@@ -16,6 +18,22 @@ const ChestSchema = new Schema({
 
   maxGold: [{ type: Number }],
 
+});
+
+ChestSchema.pre('save', next => {
+  if (this.minRare.length === 0) {
+    this.minRare = Array(numberOfArenas).fill(0);
+  }
+  if (this.minEpic.length === 0) {
+    this.minEpic = Array(numberOfArenas).fill(0);
+  }
+  if (this.minGold.length === 0) {
+    this.minGold = Array(numberOfArenas).fill(0);
+  }
+  if (this.maxGold.length === 0) {
+    this.maxGold = Array(numberOfArenas).fill(0);
+  }
+  next();
 });
 
 export default mongoose.model('Chest', ChestSchema);
