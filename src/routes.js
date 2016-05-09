3c94796a1952238a191bc793da18507d29bb130d
Martin Carrera <mcarrera.92@gmail.com>
2016-05-09 12:34:21 -0300

    Added basic information about arenas.

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router(); // eslint-disable-line new-cap
 
 import chest from './controllers/chest-controller';
+import arena from './controllers/arena-controller';
 
 router.route('/chests')
   .get(chest.find.bind(chest))
@@ -12,5 +13,16 @@ router.route('/chests/:id')
   .get(chest.findById.bind(chest))
   .put(chest.update.bind(chest))
   .delete(chest.remove.bind(chest));
+
+router.route('/arenas')
+  .get(arena.find.bind(arena))
+  .post(arena.create.bind(arena))
+  .put(arena.update.bind(arena))
+  .delete(arena.remove.bind(arena));
+
+router.route('/arenas/:id')
+  .get(arena.findById.bind(arena))
+  .put(arena.update.bind(arena))
+  .delete(arena.remove.bind(arena));
 
 export default router;
