34cdb21b57246e2b574da24d2216ab34add562db
Martin Carrera <mcarrera.92@gmail.com>
2016-05-10 15:02:59 -0300

    Fixed eslint problems

--- src/libraries/promisify-all.js
+++ src/libraries/promisify-all.js
@@ -5,6 +5,6 @@ module.exports = modules => {
   each(modules, module => {
     // @TODO: import es6 way, I don't know how to at the moment,
     // System.import doesn't seem to work
-    promisifyAll(require(module));
+    promisifyAll(require(module)); // eslint-disable-line global-require
   });
 };
