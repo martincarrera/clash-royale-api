bf2310a5f4036f26afec86af84adfe1190cfe5c6
Martin Carrera <mcarrera.92@gmail.com>
2016-05-13 17:11:40 -0300

    Added index.html

--- src/routes/main-route.js
+++ src/routes/main-route.js
@@ -1,10 +1,9 @@
 const Router = require('express').Router;
 const router = new Router();
+import path from 'path';
 
 router.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to Clash Royale API!',
-  });
+  res.sendFile(path.join(__dirname, '../../public/views', 'index.html'));
 });
 
 export default router;
