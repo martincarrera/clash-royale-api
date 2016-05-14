420509f48b757a2f2dda1e0a3b8be5eb0ad07114
Martin Carrera <mcarrera.92@gmail.com>
2016-05-14 18:34:32 -0300

    Fixed bower components error.

--- src/routes/main-route.js
+++ src/routes/main-route.js
@@ -1,8 +1,10 @@
-const Router = require('express').Router;
+const express = require('express');
+const Router = express.Router;
 const router = new Router();
 import path from 'path';
 
 router.get('/', (req, res) => {
+  res.app.use(express.static(path.join(__dirname, '../../public')));
   res.sendFile(path.join(__dirname, '../../public', 'index.html'));
 });
 
