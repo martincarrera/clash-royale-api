f48089143de44bf8b398c9ee3a365bac38f7ab55
Martin Carrera <mcarrera.92@gmail.com>
2016-05-23 13:15:17 -0300

    Implemented authentication.

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,19 +1,17 @@
 const Controller = require('../libraries/controller');
-const User = require('./user-controller');
+const User = require('../models/user-model');
 const config = require('../config/config');
 const jwt = require('jsonwebtoken');
 const secret = config.SECRET;
 
 class AuthController extends Controller {
   validate(req, res, next) {
-    console.log(req.body);
-    User.findOne({ username: req.body.username })
-    .select('name username password')
-    .exec((err, user) => {
-      if (err) next(err);
+    User.findOneWithPassword({ username: req.body.username })
+    .then(user => {
       if (!user) {
         return res.status(401).send('Failed to authenticate token.');
       }
+      console.log(user);
       const validPassword = user.comparePassword(req.body.password);
       if (!validPassword) {
         return res.status(401).send('Authentication failed. Invalid password.');
@@ -25,7 +23,8 @@ class AuthController extends Controller {
       }, secret, { expiresIn: '24h' });
 
       return res.status(200).send(token);
-    });
+    })
+    .catch(err => next(err));
   }
 }
 
--- src/models/user-model.js
+++ src/models/user-model.js
@@ -1,6 +1,13 @@
 const Model = require('../libraries/model');
 const User = require('../schemas/user-schema');
 
-class UserModel extends Model {}
+class UserModel extends Model {
+  findOneWithPassword(query, populate) {
+    return this.SchemaModel.findOne(query)
+      .select('_id username password')
+      .populate(populate || '')
+      .execAsync();
+  }
+}
 
 module.exports = new UserModel(User);
