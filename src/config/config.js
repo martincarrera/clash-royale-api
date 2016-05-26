1747e8738ba444e7798294285c37c16d665fd5a3
Martin Carrera <mcarrera.92@gmail.com>
2016-05-26 13:12:26 -0300

    Removed test config.

--- src/config/config.js
+++ src/config/config.js
@@ -1,24 +1,13 @@
 require('dotenv').load();
 
 const config = {
-  development: {
-    PORT: process.env.PORT || 8085,
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/clash-royale-api',
-    SECRET: process.env.SECRET || 'IloveClashRoyaleAPIsomuch',
-    JWT_EXPIRATION: process.env.JWT_EXPIRATION || 1440,
-    DISABLE_AUTH: process.env.DISABLE_AUTH === 'true',
-    ADMIN_USERNAME: process.env.ADMIN_USERNAME || 'admin',
-    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'admin',
-  },
-  test: {
-    PORT: process.env.PORT || 8085,
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/clash-royale-api',
-    SECRET: process.env.SECRET || 'IloveClashRoyaleAPIsomuch',
-    JWT_EXPIRATION: process.env.JWT_EXPIRATION || 1440,
-    DISABLE_AUTH: true,
-    ADMIN_USERNAME: process.env.ADMIN_USERNAME || 'admin',
-    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'admin',
-  },
+  PORT: process.env.PORT || 8085,
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/clash-royale-api',
+  SECRET: process.env.SECRET || 'IloveClashRoyaleAPIsomuch',
+  JWT_EXPIRATION: process.env.JWT_EXPIRATION || 1440,
+  DISABLE_AUTH: process.env.DISABLE_AUTH === 'true',
+  ADMIN_USERNAME: process.env.ADMIN_USERNAME || 'admin',
+  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'admin',
 };
 
-module.exports = config[process.env.NODE_ENV || 'development'];
+module.exports = config;
