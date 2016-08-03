948c83299fd8c831026dc828a4b7a93fae3fcdb9
Martin Carrera <mcarrera.92@gmail.com>
2016-08-02 23:39:15 -0300

    Add basic index.html

--- src/routes/main-route.js
+++ src/routes/main-route.js
@@ -1,18 +1,29 @@
-const Router = require('express').Router;
+const express = require('express');
+const Router = express.Router;
 const router = new Router();
+const path = require('path');
 
 router.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to Clash Royale API!',
-    endpoints: {
-      arenas: 'api/arenas',
-      cards: 'api/cards',
-      chests: 'api/chests',
-      players: 'api/players',
-    },
-    version: '1.0.0',
-    lastUpdate: 'July 04 2016',
-  });
+  res.sendFile(path.join(__dirname, '../../public', 'index.html'));
 });
 
 module.exports = router;
+//
+// const Router = require('express').Router;
+// const router = new Router();
+//
+// router.get('/', (req, res) => {
+//   res.json({
+//     message: 'Welcome to Clash Royale API!',
+//     endpoints: {
+//       arenas: 'api/arenas',
+//       cards: 'api/cards',
+//       chests: 'api/chests',
+//       players: 'api/players',
+//     },
+//     version: '1.0.0',
+//     lastUpdate: 'July 04 2016',
+//   });
+// });
+//
+// module.exports = router;
