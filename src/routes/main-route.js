bfc31c20cc675de5985eced6adbca88cca5691c4
Martín Carrera <martincarrera@users.noreply.github.com>
2017-03-23 21:17:49 -0300

    Add leagues endpoint to main call.

--- src/routes/main-route.js
+++ src/routes/main-route.js
@@ -9,6 +9,7 @@ router.get('/', (req, res) => {
       cards: 'api/cards',
       chests: 'api/chests',
       players: 'api/players',
+      leagues: 'api/leagues'
     },
     version: '1.2.0',
     lastUpdate: 'March 21 2017',
